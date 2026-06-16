68246c8f2c65793bb5b6b3c699151944b56d8407
edwinChiboy <choboy.edeh17@gmail.com>
2026-06-16 21:23:35 +0100

    templating

--- routes/admin.js
+++ routes/admin.js
@@ -1,24 +1,38 @@
-const path = require('path');
-const express = require('express');
+const path = require("path");
+const express = require("express");
 const router = express.Router();
 
-const rootDir = require('../util/path');
+const rootDir = require("../util/path");
 
-router.use("/add-product", (req, res, next) => {
-        res.status(200).sendFile(path.join(rootDir, 'views', 'add-product.html')   );
-    });
+const products = [];
 
-router.get("/update-product",(req, res, next) => {
-    res.status(200).sendFile(path.join(rootDir, 'views', 'update-product.html')   );
+router.get("/add-product", (req, res, next) => {
+  // res.status(200).sendFile(path.join(rootDir, 'views', 'add-product.html')   );
+  res.status(200).render("add-product", {
+    docTitle: "Add Product",
+    path: "/admin/add-product",
+    formCSS: true,
+    productCSS: true,
+    activeAddProduct: true,
+  });
+});
+
+router.get("/update-product", (req, res, next) => {
+  res.status(200).sendFile(path.join(rootDir, "views", "update-product.html"));
 });
 
 // app.use("/product", (req, res, next) => {
 //     console.log(req.body);
 //     res.redirect("/");
 //  });
-router.post("/product", (req, res, next) => {
-    console.log(req.body);
-    res.redirect("/");
+
+router.post("/add-product", (req, res, next) => {
+  console.log(req.body);
+  products.push({ title: req.body.title });
+  res.redirect("/");
 });
- 
-module.exports = router;
+
+// module.exports = router;
+
+exports.routes = router;
+exports.products = products;
