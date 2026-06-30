8aef40e6ddaa7d116dd73982a16ac37d3dbfb3da
edwinChiboy <choboy.edeh17@gmail.com>
2026-06-30 11:24:28 +0100

    async

--- controllers/shop.js
+++ controllers/shop.js
@@ -499,6 +499,29 @@ exports.getOrders = (req, res, next) => {
 //     .catch((err) => console.log(err));
 // };
 
+exports.getCheckout = (req, res, next) => {
+  req.user
+    .populate("cart.items.productId")
+    .then((user) => {
+      const products = user.cart.items;
+      let total = 0;
+      products.forEach((p) => {
+        total += p.quantity * p.productId.price;
+      });
+      res.render("shop/checkout", {
+        path: "/checkout",
+        pageTitle: "Checkout",
+        products: products,
+        totalSum: total,
+      });
+    })
+    .catch((err) => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
+    });
+};
+
 exports.postOrder = (req, res, next) => {
   req.user
     .populate("cart.items.productId")
